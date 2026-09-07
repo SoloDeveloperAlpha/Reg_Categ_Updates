7ee5f642adf58e71c0389b99005a623d79b81b41
Walter Astudillo Zevallos <112835682+SoloDeveloperAlpha@users.noreply.github.com>
2026-09-07 21:45:45 +0000

    Cambios realizados

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
-
 const PORT = 3000;
+const frontendPath = path.join(__dirname, '../frontend');
+
+app.use(express.static(frontendPath));
 
 app.get('/', (req, res) => {
-  res.send('Servidor funcionando correctamente');
+  res.sendFile(path.join(frontendPath, 'login.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(frontendPath, 'login.html'));
+});
+
+app.get('/dashboard', (req, res) => {
+  res.sendFile(path.join(frontendPath, 'index.html'));
 });
 
 app.listen(PORT, () => {
